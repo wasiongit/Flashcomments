68e396696dad507389601191a55107fa29008a63
Nelwhix <nelsonisioma1@gmail.com>
2022-06-16 06:03:07 +0100

    cool

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -11,14 +11,15 @@ class CartController extends Controller
 {
     public $user_request;
     
-    public function list() {
-        dd($this->user_request);
-    }
     public function search(Request $request) {
         $this->user_request = $request->all();
         $sum = array_sum($request->all());
         return view('checkout', [
             'comments_num' => $sum
         ]);
-}
+    }
+
+    public function list() {
+        dd($this->user_request);
+    }
 }
